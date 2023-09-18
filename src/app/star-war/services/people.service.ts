305f2783d6a036f3e650932568e91ab6943a666f
Kritsada Intaprom <kritsada.itp@gmail.com>
2023-09-18 17:45:33 +0700

    week-08

---
+++ src/app/star-war/services/people.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { parsePeopleList } from '../helper';
+import { List, Person, RawList, RawPerson, SearchData } from '../models';
+import { Observable, map } from 'rxjs';
+
+const url = 'https://swapi.dev/api/people' as const;
+@Injectable({
+  providedIn: 'root'
+})
+export class PeopleService {
+  private readonly http = inject(HttpClient);
+  queryParams: any;
+
+  getAll(params?: SearchData): Observable<List<Person>>{
+    return (
+    this.http
+    .get<RawList<RawPerson>>(url,{params: params})
+    .pipe(map(parsePeopleList))
+    );
+  }
+
+}
